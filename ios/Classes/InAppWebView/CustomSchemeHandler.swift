b1946be6d9092668e428ff9bf82ca1a4325d5dd3
MorningStar <jngviho@icloud.com>
2023-05-18 11:02:07 +0800

    hook ios http

--- ios/Classes/InAppWebView/CustomSchemeHandler.swift
+++ ios/Classes/InAppWebView/CustomSchemeHandler.swift
@@ -12,31 +12,64 @@ import WebKit
 @available(iOS 11.0, *)
 public class CustomSchemeHandler : NSObject, WKURLSchemeHandler {
     var schemeHandlers: [Int:WKURLSchemeTask] = [:]
-    
+    static var proxyHost: String?
+    static var proxyPort: Int?
+
     public func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
         schemeHandlers[urlSchemeTask.hash] = urlSchemeTask
-        let inAppWebView = webView as! InAppWebView
-        let request = WebResourceRequest.init(fromURLRequest: urlSchemeTask.request)
-        let callback = WebViewChannelDelegate.LoadResourceWithCustomSchemeCallback()
-        callback.nonNullSuccess = { (response: CustomSchemeResponse) in
-            if (self.schemeHandlers[urlSchemeTask.hash] != nil) {
-                let urlResponse = URLResponse(url: request.url, mimeType: response.contentType, expectedContentLength: -1, textEncodingName: response.contentEncoding)
-                urlSchemeTask.didReceive(urlResponse)
-                urlSchemeTask.didReceive(response.data)
-                urlSchemeTask.didFinish()
-                self.schemeHandlers.removeValue(forKey: urlSchemeTask.hash)
+        Task {
+            do {
+                let configuration = URLSessionConfiguration.default
+                if let host = CustomSchemeHandler.proxyHost,
+                   let port = CustomSchemeHandler.proxyPort {
+                    configuration.connectionProxyDictionary = [
+                        "HTTPEnable": true,
+                        "HTTPProxy": host,
+                        "HTTPPort": port,
+                        "HTTPSEnable": true,
+                        "HTTPSProxy": host,
+                        "HTTPSPort": port,
+                    ]
+                }
+               
+                configuration.timeoutIntervalForRequest = 5
+                configuration.timeoutIntervalForResource = 5
+                let session = URLSession(configuration: configuration)
+                
+                let (data, response) = try await session.data(for: urlSchemeTask.request)
+                if (self.schemeHandlers[urlSchemeTask.hash] != nil) {
+                    urlSchemeTask.didReceive(response)
+                    urlSchemeTask.didReceive(data)
+                    urlSchemeTask.didFinish()
+                }
+                
+            } catch {
+                print("task: url:\(urlSchemeTask.request.url?.absoluteString ?? "unknown url") error: \(error)")
             }
-            return false
+            schemeHandlers.removeValue(forKey: urlSchemeTask.hash)
         }
-        callback.error = { (code: String, message: String?, details: Any?) in
-            print(code + ", " + (message ?? ""))
-        }
+        // let inAppWebView = webView as! InAppWebView
+        // let request = WebResourceRequest.init(fromURLRequest: urlSchemeTask.request)
+        // let callback = WebViewChannelDelegate.LoadResourceWithCustomSchemeCallback()
+        // callback.nonNullSuccess = { (response: CustomSchemeResponse) in
+        //     if (self.schemeHandlers[urlSchemeTask.hash] != nil) {
+        //         let urlResponse = URLResponse(url: request.url, mimeType: response.contentType, expectedContentLength: -1, textEncodingName: response.contentEncoding)
+        //         urlSchemeTask.didReceive(urlResponse)
+        //         urlSchemeTask.didReceive(response.data)
+        //         urlSchemeTask.didFinish()
+        //         self.schemeHandlers.removeValue(forKey: urlSchemeTask.hash)
+        //     }
+        //     return false
+        // }
+        // callback.error = { (code: String, message: String?, details: Any?) in
+        //     print(code + ", " + (message ?? ""))
+        // }
         
-        if let channelDelegate = inAppWebView.channelDelegate {
-            channelDelegate.onLoadResourceWithCustomScheme(request: request, callback: callback)
-        } else {
-            callback.defaultBehaviour(nil)
-        }
+        // if let channelDelegate = inAppWebView.channelDelegate {
+        //     channelDelegate.onLoadResourceWithCustomScheme(request: request, callback: callback)
+        // } else {
+        //     callback.defaultBehaviour(nil)
+        // }
     }
     
     public func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
---
+++ ios/Classes/WKWebView+HandleScheme.h
@@ -0,0 +1,17 @@
+//
+//  WKWebView+HandleScheme.h
+//  WKProxy
+//
+//  Created by MorningStar on 2023/5/11.
+//
+
+#import <WebKit/WebKit.h>
+//#import "flutter_inappwebview-Swift.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKWebView (HandleScheme)
+
+@end
+
+NS_ASSUME_NONNULL_END
